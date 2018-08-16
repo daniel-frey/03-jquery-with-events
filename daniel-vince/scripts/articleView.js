16929a97bfe708bf4e236352bfb5f1792875dd66
Vince Masten <vmasten@me.com>
2018-08-16 11:23:52 -0700

    added category filter

--- daniel-vince/scripts/articleView.js
+++ daniel-vince/scripts/articleView.js
@@ -56,7 +56,21 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
+    $('article').show();
+    if ($(this).val()) {
+      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).show();
+    } else {
+      $('article').show();
+      $('article.template').hide();
+      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
